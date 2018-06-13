072e61f4b2215e29000eb00ad69f6d60adef503c
Shahid Sani <sdaura1@gmail.com>
2018-06-14 00:20:16 +0100

    SignIn Activity 2

--- app/src/main/java/com/example/soosyamoora/devzone/SignIn_Activity.java
+++ app/src/main/java/com/example/soosyamoora/devzone/SignIn_Activity.java
@@ -1,4 +1,13 @@
 package com.example.soosyamoora.devzone;
 
-public class SignIn_Activity {
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class SignIn_Activity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+    }
 }
