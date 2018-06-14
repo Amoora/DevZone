25527723b49cd3aa6b15dde237e52a980b076480
Shahid Sani <sdaura1@gmail.com>
2018-06-14 13:57:04 +0100

    Ask Question Firebase 14/06/2018

--- app/src/main/java/com/example/soosyamoora/devzone/Ask_Question_Activity.java
+++ app/src/main/java/com/example/soosyamoora/devzone/Ask_Question_Activity.java
@@ -1,6 +1,7 @@
 package com.example.soosyamoora.devzone;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -11,12 +12,23 @@ import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreSettings;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class Ask_Question_Activity extends AppCompatActivity {
 
     Spinner question_type;
     Button ask_button;
     EditText question_editText;
+    FirebaseFirestore firestoreRef;
+    FirebaseFirestoreSettings settings;
     ArrayAdapter<CharSequence> adapter;
+    Map<String, Object> firestoreMap;
     String question_type_string, question_string;
 
     @Override
@@ -27,6 +39,11 @@ public class Ask_Question_Activity extends AppCompatActivity {
         question_type = findViewById(R.id.question_type_spinner);
         question_editText = findViewById(R.id.question_editText);
         ask_button = findViewById(R.id.ask_button);
+        settings = new FirebaseFirestoreSettings.Builder()
+                .setPersistenceEnabled(true)
+                .build();
+        firestoreRef = FirebaseFirestore.getInstance();
+        firestoreMap = new HashMap<>();
 
         adapter = ArrayAdapter.createFromResource(this, R.array.question_types,
                 android.R.layout.simple_spinner_item);
@@ -43,8 +60,39 @@ public class Ask_Question_Activity extends AppCompatActivity {
         ask_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                question_string = question_editText.getText().toString();
-                Toast.makeText(getApplicationContext(), "you asked " + question_string + " that is a type of " + question_type_string, Toast.LENGTH_LONG).show();
+
+                if (question_editText == null) {
+                    Toast.makeText(getApplicationContext(), "All Fields are required", Toast.LENGTH_SHORT).show();
+                } else {
+                    question_string = question_editText.getText().toString();
+                    Toast.makeText(getApplicationContext(),
+                            "you asked " + question_string + " that is a type of " + question_type_string,
+                            Toast.LENGTH_LONG).show();
+
+                    firestoreMap.put("Question Type", question_type_string);
+                    firestoreMap.put("Question", question_string);
+
+                    firestoreRef.collection("ASKED QUESTIONS")
+                            .document().set(firestoreMap)
+                            .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if (task.isSuccessful()) {
+                                        Toast.makeText(getApplicationContext(),
+                                                "Your Question is sent, you will be notified when answered, Thanks",
+                                                Toast.LENGTH_LONG).show();
+
+                                        question_editText.setText("");
+                                    } else {
+                                        Toast.makeText(getApplicationContext(),
+                                                "Your Question is not sent please connect to a network, Thanks",
+                                                Toast.LENGTH_LONG).show();
+
+                                    }
+                                }
+                            });
+
+                }
             }
         });
     }
